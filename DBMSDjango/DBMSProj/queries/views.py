6a18ee0617f4a458aae25c4a7476f93dd74c34f5
connor <connorskastner@gmail.com>
2023-08-24 15:43:47 -0500

    Fix bugs

--- DBMSDjango/DBMSProj/queries/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import JsonResponse
-from django.template import loader
-import mariadb
-
-def index(request):
-    return render(request, 'index.html')
